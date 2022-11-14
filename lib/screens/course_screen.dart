a7e9c8dcba6a4f6e53a8bc55c622771a315d5143
freshnoob1e <jazchanzj@gmail.com>
2022-11-14 19:44:41 +0800

    update course detail

--- lib/screens/course_screen.dart
+++ lib/screens/course_screen.dart
@@ -16,13 +16,19 @@ class CourseScreen extends StatefulWidget {
 }
 
 class _CourseScreenState extends State<CourseScreen> {
+  List<double> lessonHoursDuration = [];
   final GlobalKey<FormState> _newCourseFormKey = GlobalKey<FormState>();
   final newCourseCodeController = TextEditingController();
   final newCourseDescController = TextEditingController();
   Map<ClassType, double> newCourseLessonHour = {};
-  List<double> lessonHoursDuration = [];
   Lecturer? newCourseLect;
   Programme? newCourseProg;
+  final GlobalKey<FormState> _editCourseFormKey = GlobalKey<FormState>();
+  final editCourseCodeController = TextEditingController();
+  final editCourseDescController = TextEditingController();
+  Map<ClassType, double> editCourseLessonHour = {};
+  Lecturer? editCourseLect;
+  Programme? editCourseProg;
 
   @override
   void initState() {
@@ -35,6 +41,9 @@ class _CourseScreenState extends State<CourseScreen> {
   @override
   void dispose() {
     newCourseCodeController.dispose();
+    newCourseDescController.dispose();
+    editCourseCodeController.dispose();
+    editCourseDescController.dispose();
     super.dispose();
   }
 
@@ -99,25 +108,119 @@ class _CourseScreenState extends State<CourseScreen> {
       ),
       // Course lesson hour
       ...List.generate(ClassType.values.length, (index) {
-        return Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(ClassType.values[index].name),
-            DropdownButton<double>(
-              value: lessonHoursDuration[0],
-              items: lessonHoursDuration.map((e) {
-                return DropdownMenuItem<double>(
-                  value: e,
-                  child: Text("${e.toString()} hours"),
-                );
-              }).toList(),
-              onChanged: (value) {
-                if (value != null) {
-                  newCourseLessonHour[ClassType.values[index]] = value;
-                }
-              },
+        return DropdownButtonFormField(
+          decoration: InputDecoration(
+            label: Text(ClassType.values[index].name),
+          ),
+          value: lessonHoursDuration[0],
+          items: lessonHoursDuration.map((e) {
+            return DropdownMenuItem<double>(
+              value: e,
+              child: Text("${e.toString()} hours"),
+            );
+          }).toList(),
+          onChanged: (value) {
+            if (value != null) {
+              newCourseLessonHour[ClassType.values[index]] = value;
+            }
+          },
+        );
+      })
+    ];
+  }
+
+  Future<List<Widget>> editCourseDialogForm(int courseID) async {
+    List<Lecturer> lects = await LecturerRepository.retrieveLecturers();
+    List<Programme> progs = await ProgrammeRepository.retrieveProgrammes();
+
+    Course editCourse = await CourseRepository.retrieveCourseById(courseID);
+    for (Lecturer lect in lects) {
+      if (lect.id == editCourse.lecturer.id) {
+        editCourseLect = lect;
+      }
+    }
+    for (Programme prog in progs) {
+      if (prog.id == editCourse.programmeCode.id) {
+        editCourseProg = prog;
+      }
+    }
+    editCourseCodeController.text = editCourse.courseCode;
+    editCourseDescController.text = editCourse.courseDescription ?? "";
+    editCourse.lessonsHours.forEach((key, value) {
+      editCourseLessonHour.addAll({key: value});
+    });
+
+    return [
+      // Course's lecturer
+      DropdownButtonFormField(
+        value: editCourseLect,
+        items: lects.map((Lecturer e) {
+          return DropdownMenuItem<Lecturer>(
+            value: e,
+            child: Text(e.name),
+          );
+        }).toList(),
+        onChanged: (value) {
+          if (value != null) {
+            editCourseLect = value;
+          }
+        },
+      ),
+      // Course's programme
+      DropdownButtonFormField(
+        value: editCourseProg,
+        items: progs.map((Programme e) {
+          return DropdownMenuItem<Programme>(
+            value: e,
+            child: Text(e.programmeCode),
+          );
+        }).toList(),
+        onChanged: (value) {
+          if (value != null) {
+            editCourseProg = value;
+          }
+        },
+      ),
+      // Course Code
+      TextFormField(
+        controller: editCourseCodeController,
+        decoration: const InputDecoration(
+          hintText: "Course Code (e.x. BAIT1234)",
+        ),
+        validator: (value) {
+          if (value == null || value.isEmpty) {
+            return "Please enter a valid course code";
+          }
+          return null;
+        },
+      ),
+      // Course description
+      TextFormField(
+        controller: editCourseDescController,
+        decoration: const InputDecoration(
+          hintText: "Course Description",
+        ),
+      ),
+      // Course lesson hour
+      ...List.generate(ClassType.values.length, (index) {
+        return DropdownButtonFormField(
+          decoration: InputDecoration(
+            label: Text(
+              ClassType.values[index].name,
             ),
-          ],
+          ),
+          value: editCourseLessonHour[ClassType.values[index]],
+          items: lessonHoursDuration.map((e) {
+            return DropdownMenuItem(
+              value: e,
+              child: Text("${e.toString()} hours"),
+            );
+          }).toList(),
+          onChanged: (value) {
+            if (value != null) {
+              editCourseLessonHour[ClassType.values[index]] = value;
+            }
+          },
         );
       })
     ];
@@ -144,6 +247,31 @@ class _CourseScreenState extends State<CourseScreen> {
     setState(() {});
     navigator.pop();
     newCourseCodeController.clear();
+    newCourseDescController.clear();
+  }
+
+  Future updateCourse(int courseID) async {
+    if (!_editCourseFormKey.currentState!.validate()) return;
+
+    final navigator = Navigator.of(context);
+
+    if (editCourseLect == null) return;
+    if (editCourseProg == null) return;
+
+    await CourseRepository.updateCourse(
+      Course(
+        courseID,
+        editCourseLect!,
+        editCourseProg!,
+        editCourseCodeController.text,
+        editCourseDescController.text,
+        editCourseLessonHour,
+      ),
+    );
+    setState(() {});
+    navigator.pop();
+    editCourseCodeController.clear();
+    editCourseDescController.clear();
   }
 
   Future removeCourse(int courseId) async {
@@ -246,57 +374,109 @@ class _CourseScreenState extends State<CourseScreen> {
                                       MainAxisAlignment.spaceBetween,
                                   children: [
                                     Text("${index + 1}. ${course.courseCode}"),
-                                    IconButton(
-                                      onPressed: () => showDialog(
-                                        context: context,
-                                        builder: (context) {
-                                          return AlertDialog(
-                                            content: Column(
-                                              children: [
-                                                const Center(
-                                                  child:
-                                                      Text("Confirm Delete?"),
+                                    Row(
+                                      children: [
+                                        IconButton(
+                                          onPressed: () => showDialog(
+                                            context: context,
+                                            builder: (context) {
+                                              return AlertDialog(
+                                                content: FutureBuilder(
+                                                  future: editCourseDialogForm(
+                                                    course.id!,
+                                                  ),
+                                                  builder: (context, snapshot) {
+                                                    if (snapshot
+                                                            .connectionState !=
+                                                        ConnectionState.done) {
+                                                      return Container();
+                                                    }
+                                                    if (!snapshot.hasData) {
+                                                      return Container();
+                                                    }
+                                                    return Form(
+                                                      key: _editCourseFormKey,
+                                                      child: Column(
+                                                        children: [
+                                                          ...snapshot.data!,
+                                                          ElevatedButton(
+                                                            onPressed:
+                                                                () async {
+                                                              await updateCourse(
+                                                                  course.id!);
+                                                            },
+                                                            child: const Text(
+                                                                "Update Course"),
+                                                          ),
+                                                        ],
+                                                      ),
+                                                    );
+                                                  },
                                                 ),
-                                                const SizedBox(
-                                                  height: 50,
-                                                ),
-                                                Row(
-                                                  mainAxisAlignment:
-                                                      MainAxisAlignment.center,
+                                              );
+                                            },
+                                          ),
+                                          icon: const Icon(Icons.edit),
+                                        ),
+                                        IconButton(
+                                          onPressed: () => showDialog(
+                                            context: context,
+                                            builder: (context) {
+                                              return AlertDialog(
+                                                content: Column(
                                                   children: [
-                                                    ElevatedButton(
-                                                      style: const ButtonStyle(
-                                                        backgroundColor:
-                                                            MaterialStatePropertyAll(
-                                                          Colors.red,
-                                                        ),
-                                                      ),
-                                                      onPressed: () =>
-                                                          Navigator.pop(
-                                                        context,
-                                                      ),
-                                                      child: const Text("NO"),
+                                                    const Center(
+                                                      child: Text(
+                                                          "Confirm Delete?"),
                                                     ),
                                                     const SizedBox(
-                                                      width: 20,
+                                                      height: 50,
                                                     ),
-                                                    ElevatedButton(
-                                                      onPressed: () {
-                                                        removeCourse(
-                                                          course.id!,
-                                                        );
-                                                        Navigator.pop(context);
-                                                      },
-                                                      child: const Text("YES"),
+                                                    Row(
+                                                      mainAxisAlignment:
+                                                          MainAxisAlignment
+                                                              .center,
+                                                      children: [
+                                                        ElevatedButton(
+                                                          style:
+                                                              const ButtonStyle(
+                                                            backgroundColor:
+                                                                MaterialStatePropertyAll(
+                                                              Colors.red,
+                                                            ),
+                                                          ),
+                                                          onPressed: () =>
+                                                              Navigator.pop(
+                                                            context,
+                                                          ),
+                                                          child:
+                                                              const Text("NO"),
+                                                        ),
+                                                        const SizedBox(
+                                                          width: 20,
+                                                        ),
+                                                        ElevatedButton(
+                                                          onPressed: () {
+                                                            removeCourse(
+                                                              course.id!,
+                                                            );
+                                                            Navigator.pop(
+                                                                context);
+                                                          },
+                                                          child:
+                                                              const Text("YES"),
+                                                        ),
+                                                      ],
                                                     ),
                                                   ],
                                                 ),
-                                              ],
-                                            ),
-                                          );
-                                        },
-                                      ),
-                                      icon: const Icon(Icons.delete_forever),
+                                              );
+                                            },
+                                          ),
+                                          icon:
+                                              const Icon(Icons.delete_forever),
+                                        ),
+                                      ],
                                     ),
                                   ],
                                 ),
