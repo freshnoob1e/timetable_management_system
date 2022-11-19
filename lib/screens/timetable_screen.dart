f342b2cc9c491f9d20a2f9a448709b11a13e6427
freshnoob1e <jazchanzj@gmail.com>
2022-11-19 23:43:03 +0800

    Filter timetable sessions

--- lib/screens/timetable_screen.dart
+++ lib/screens/timetable_screen.dart
@@ -11,6 +11,8 @@ import 'package:timetable_management_system/genetic_algorithm/population.dart';
 import 'package:timetable_management_system/genetic_algorithm/scheduler.dart';
 import 'package:timetable_management_system/model/class_session.dart';
 import 'package:timetable_management_system/model/course.dart';
+import 'package:timetable_management_system/model/lecturer.dart';
+import 'package:timetable_management_system/model/programme.dart';
 import 'package:timetable_management_system/model/timeslot.dart';
 import 'package:timetable_management_system/model/venue.dart';
 import 'package:timetable_management_system/repository/app_setting_repository.dart';
@@ -34,6 +36,12 @@ class TimetableScreen extends StatefulWidget {
 class _TimetableScreenState extends State<TimetableScreen> {
   Scheduler scheduler = Scheduler();
   bool generatedTimetable = false;
+  List<ClassSession> sessions = [];
+  int currentFilterType = 0;
+  String currentVenueVal = "";
+  String currentCourseVal = "";
+  String currentProgVal = "";
+  String currentLectVal = "";
 
   Future generateTimetable(List<Course> courses, List<Venue> venues) async {
     scheduler = Scheduler();
@@ -71,8 +79,8 @@ class _TimetableScreenState extends State<TimetableScreen> {
     for (var event in calendarControllerProvider.controller.events) {
       calendarControllerProvider.controller.remove(event);
     }
-
-    List<ClassSession> sessions = scheduler.fittestTimetableClassSessions();
+    sessions = [];
+    sessions = scheduler.fittestTimetableClassSessions();
 
     for (ClassSession session in sessions) {
       String classTypeStr = "";
@@ -118,6 +126,83 @@ class _TimetableScreenState extends State<TimetableScreen> {
     setState(() {});
   }
 
+  void refreshTimetableWithFilter() {
+    if (currentFilterType == 0) {
+      refreshTimetable();
+      return;
+    }
+    CalendarControllerProvider calendarControllerProvider =
+        CalendarControllerProvider.of(context);
+    for (var event in calendarControllerProvider.controller.events) {
+      calendarControllerProvider.controller.remove(event);
+    }
+
+    List<ClassSession> filteredSession = [];
+
+    for (ClassSession session in sessions) {
+      if (currentFilterType == 1) {
+        if (session.venue.venueName == currentVenueVal) {
+          filteredSession.add(session);
+        }
+      } else if (currentFilterType == 2) {
+        if (session.course.courseCode == currentCourseVal) {
+          filteredSession.add(session);
+        }
+      } else if (currentFilterType == 3) {
+        if (session.course.programmeCode.programmeCode == currentProgVal) {
+          filteredSession.add(session);
+        }
+      } else {
+        if (session.course.lecturer.name == currentLectVal) {
+          filteredSession.add(session);
+        }
+      }
+    }
+
+    for (ClassSession session in filteredSession) {
+      String classTypeStr = "";
+      switch (session.classType) {
+        case ClassType.lecture:
+          classTypeStr = "L";
+          break;
+        case ClassType.tutorial:
+          classTypeStr = "T";
+          break;
+        case ClassType.practical:
+          classTypeStr = "P";
+          break;
+        case ClassType.blended:
+          classTypeStr = "B";
+          break;
+        default:
+          classTypeStr = "Unknown";
+          break;
+      }
+      String eventStr =
+          "$classTypeStr, ${session.venue.venueName}&${session.course.programmeCode.programmeCode}, ${session.course.lecturer.name}";
+      bool hasClash = false;
+      if (session.isAnyHardClash(filteredSession)) {
+        hasClash = true;
+      }
+      if (hasClash) {
+        eventStr += "&true";
+      } else {
+        eventStr += "&false";
+      }
+
+      final event = CalendarEventData(
+        title: session.course.courseCode,
+        event: eventStr,
+        date: session.startTime,
+        endDate: session.endTime,
+        startTime: session.startTime,
+        endTime: session.endTime,
+      );
+      calendarControllerProvider.controller.add(event);
+    }
+    setState(() {});
+  }
+
   Future<File> localFile() async {
     Directory appDocDir = await getApplicationSupportDirectory();
     String appDocPath = appDocDir.path;
@@ -128,7 +213,8 @@ class _TimetableScreenState extends State<TimetableScreen> {
     try {
       Map<String, Map<String, dynamic>> jsonMap = {};
 
-      List<ClassSession> sessions = scheduler.fittestTimetableClassSessions();
+      sessions = [];
+      sessions = scheduler.fittestTimetableClassSessions();
 
       int i = 0;
       for (var session in sessions) {
@@ -159,7 +245,7 @@ class _TimetableScreenState extends State<TimetableScreen> {
       final String jsonContent = await file.readAsString();
       Map<String, dynamic> map = json.decode(jsonContent);
 
-      List<ClassSession> sessions = [];
+      sessions = [];
       map.forEach((key, value) {
         sessions.add(ClassSession.fromJson(value));
       });
@@ -212,6 +298,139 @@ class _TimetableScreenState extends State<TimetableScreen> {
       EasyLoading.showError("Something went wrong...");
       return 0;
     }
+  }
+
+  DropdownButton? filterTypeDD() {
+    switch (currentFilterType) {
+      case 1:
+        return venueDDMenuItemWidgets();
+      case 2:
+        return courseDDMenuItemWidgets();
+      case 3:
+        return progDDMenuItemWidgets();
+      case 4:
+        return lecturerDDMenuItemWidgets();
+      default:
+        return null;
+    }
+  }
+
+  DropdownButton venueDDMenuItemWidgets() {
+    List<String> venuesName = [];
+    for (ClassSession session in sessions) {
+      venuesName.add(session.venue.venueName);
+    }
+    venuesName = venuesName.toSet().toList();
+    if (currentVenueVal == "") {
+      currentVenueVal = venuesName[0];
+    }
+    return DropdownButton(
+      value: currentVenueVal,
+      items: List.generate(
+        venuesName.length,
+        (index) => DropdownMenuItem(
+          value: venuesName[index],
+          child: Text(venuesName[index]),
+        ),
+      ),
+      onChanged: (value) {
+        setState(() {
+          currentVenueVal = value;
+          refreshTimetableWithFilter();
+        });
+      },
+    );
+  }
+
+  DropdownButton courseDDMenuItemWidgets() {
+    List<String> coursesCodes = [];
+    for (ClassSession session in sessions) {
+      coursesCodes.add(session.course.courseCode);
+    }
+    coursesCodes = coursesCodes.toSet().toList();
+    if (currentCourseVal == "") {
+      currentCourseVal = coursesCodes[0];
+    }
+    return DropdownButton(
+      value: currentCourseVal,
+      items: List.generate(
+        coursesCodes.length,
+        (index) => DropdownMenuItem(
+          value: coursesCodes[index],
+          child: Text(coursesCodes[index]),
+        ),
+      ),
+      onChanged: (value) {
+        setState(() {
+          currentCourseVal = value;
+          refreshTimetableWithFilter();
+        });
+      },
+    );
+  }
+
+  DropdownButton progDDMenuItemWidgets() {
+    List<String> progsCodes = [];
+    for (ClassSession session in sessions) {
+      progsCodes.add(session.course.programmeCode.programmeCode);
+    }
+    progsCodes = progsCodes.toSet().toList();
+    if (currentProgVal == "") {
+      currentProgVal = progsCodes[0];
+    }
+    return DropdownButton(
+      value: currentProgVal,
+      items: List.generate(
+        progsCodes.length,
+        (index) => DropdownMenuItem(
+          value: progsCodes[index],
+          child: Text(progsCodes[index]),
+        ),
+      ),
+      onChanged: (value) {
+        setState(() {
+          currentProgVal = value;
+          refreshTimetableWithFilter();
+        });
+      },
+    );
+  }
+
+  DropdownButton lecturerDDMenuItemWidgets() {
+    List<String> lecturersNames = [];
+    for (ClassSession session in sessions) {
+      lecturersNames.add(session.course.lecturer.name);
+    }
+    lecturersNames = lecturersNames.toSet().toList();
+    if (currentLectVal == "") {
+      currentLectVal = lecturersNames[0];
+    }
+    return DropdownButton(
+      value: currentLectVal,
+      items: List.generate(
+        lecturersNames.length,
+        (index) => DropdownMenuItem(
+          value: lecturersNames[index],
+          child: Text(lecturersNames[index]),
+        ),
+      ),
+      onChanged: (value) {
+        setState(() {
+          currentLectVal = value;
+          refreshTimetableWithFilter();
+        });
+      },
+    );
+  }
+
+  void resetFilter() {
+    setState(() {
+      currentFilterType = 0;
+      currentVenueVal = "";
+      currentCourseVal = "";
+      currentProgVal = "";
+      currentLectVal = "";
+    });
   }
 
   @override
@@ -294,6 +513,7 @@ class _TimetableScreenState extends State<TimetableScreen> {
                 ),
                 ElevatedButton(
                   onPressed: () async {
+                    resetFilter();
                     EasyLoading.show(status: "Generating...");
 
                     List<Course> courses =
@@ -329,6 +549,48 @@ class _TimetableScreenState extends State<TimetableScreen> {
                       )
                     : Container(),
               ],
+            ),
+            sessions.isNotEmpty
+                ? DropdownButton(
+                    value: currentFilterType,
+                    items: const [
+                      DropdownMenuItem(
+                        value: 0,
+                        child: Text("All"),
+                      ),
+                      DropdownMenuItem(
+                        value: 1,
+                        child: Text("Venue"),
+                      ),
+                      DropdownMenuItem(
+                        value: 2,
+                        child: Text("Course"),
+                      ),
+                      DropdownMenuItem(
+                        value: 3,
+                        child: Text("Programme"),
+                      ),
+                      DropdownMenuItem(
+                        value: 4,
+                        child: Text("Lecturer"),
+                      ),
+                    ],
+                    onChanged: (value) {
+                      setState(() {
+                        if (value == 0) {
+                          refreshTimetable();
+                        }
+                        currentFilterType = value ?? 0;
+                      });
+                    },
+                  )
+                : Container(),
+            const SizedBox(
+              height: 15,
+            ),
+            filterTypeDD() ?? Container(),
+            const SizedBox(
+              height: 15,
             ),
             Expanded(
               child: WeekView(
